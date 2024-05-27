c54f3c0cca67eafbeb4f27730aeadafc967bfb2e
Jon Stelman <jon@jonstelman.com>
2024-05-27 13:00:18 -0400

    add logger

---
+++ src/logger.py
@@ -0,0 +1,13 @@
+import logging
+import logging.config
+import json
+
+def setup_logging(config_file="logging_config.json"):
+    """Creates a new instance of the logger configuration"""
+    with open(config_file, "r", encoding="utf-8") as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
+
+# Initialize the logger
+setup_logging()
+logger = logging.getLogger(__name__)
